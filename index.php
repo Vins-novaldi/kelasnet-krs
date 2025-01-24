50228c41192b91f0d60d10bbe9883a087c29d828
Vins-novaldi <vijjaynovaldi01@gmail.com>
2025-01-24 21:16:58 +0700

    merubah file index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
-require_once 'config/koneksi.php'; 
+require_once 'koneksi.php'; 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-include('pages/input-mhs.php');
+include('mahasiswapage/login.php');
 ?>
